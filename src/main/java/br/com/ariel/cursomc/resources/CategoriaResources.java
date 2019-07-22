6d2934e19b55f1a2328a5066ee2c34f9568c629e
ariel montovani <am.si@outlook.com>
2019-07-21 22:47:15 -0300

    testando a primeira classe de domineo - categoria

--- src/main/java/br/com/ariel/cursomc/resources/CategoriaResources.java
+++ src/main/java/br/com/ariel/cursomc/resources/CategoriaResources.java
@@ -1,18 +1,29 @@
 package br.com.ariel.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ariel.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResources {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "funciono";
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
 		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
