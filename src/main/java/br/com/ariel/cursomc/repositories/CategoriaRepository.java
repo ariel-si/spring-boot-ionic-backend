1962e7e4bac791d8463a6cf3c43578d6a69e42eb
ariel montovani <am.si@outlook.com>
2019-07-22 23:56:56 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/br/com/ariel/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package br.com.ariel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.ariel.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+
+}
